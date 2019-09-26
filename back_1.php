12bbdc78a02c8b126bfb7fe9aba77536381af3ad
Larry <pascu.laurentiu17@gmail.com>
2019-09-26 07:38:09 +0300

    create backend task 1

---
+++ back_1.php
@@ -0,0 +1,55 @@
+<?php
+
+$carsArray = array(
+    array(
+        'brand' => 'volvo',
+        'model' => 's80',
+        'seats' => 5,
+        'color' => 'Grey',
+        'year' => '2002',
+        'country' => 'Sweden'
+    ),
+    array(
+        'brand' => 'audi',
+        'model' => 'TT',
+        'seats' => 2,
+        'color' => 'Red',
+        'year' => '2007',
+        'country' => 'Germany'
+    ),
+    array(
+        'brand' => 'volkswagen',
+        'model' => 'golf mk2',
+        'seats' => 4,
+        'color' => 'blue',
+        'year' => '2004',
+        'country' => 'Germany'
+    ),
+    array(
+        'brand' => 'ford',
+        'model' => 'focus',
+        'seats' => 5,
+        'color' => 'white',
+        'year' => '2015',
+        'country' => 'USA'
+    ),
+    array(
+        'brand' => 'bmw',
+        'model' => '320d',
+        'seats' => 5,
+        'color' => 'Grey',
+        'year' => '2010',
+        'country' => 'Germany'
+    ),
+    array(
+        'brand' => 'renault',
+        'model' => 'megane',
+        'seats' => 5,
+        'color' => 'blue',
+        'year' => '2009',
+        'country' => 'France'
+    )
+);
+
+echo '<pre>';
+print_r($carsArray);
